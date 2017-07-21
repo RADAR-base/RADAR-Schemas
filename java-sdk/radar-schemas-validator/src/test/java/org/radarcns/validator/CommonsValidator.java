6b1ce7d53bec082690b392b0f3a1be7f72512ac5
Francesco Nobilia <f.nobilia@gmail.com>
2017-07-21 18:12:06 +0100

    Fixed key name files and added first unit tests

---
+++ java-sdk/radar-schemas-validator/src/test/java/org/radarcns/validator/CommonsValidator.java
@@ -0,0 +1,63 @@
+package org.radarcns.validator;
+
+/*
+ * Copyright 2017 King's College London and The Hyve
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.IOException;
+import org.radarcns.validator.StructureValidator.CommonsFolder;
+import org.radarcns.validator.StructureValidator.NameFolder;
+
+/** TODO. */
+public final class CommonsValidator {
+
+    //private static final Logger LOGGER = LoggerFactory.getLogger(CommonsValidator.class);
+
+    private CommonsValidator() {
+        //Static class
+    }
+
+    /** TODO. */
+    public static void validateAll() throws IOException {
+        active();
+        kafka();
+        monitor();
+        passive();
+    }
+
+    /** TODO. */
+    public static void active() throws IOException {
+        AvroValidator.analiseFiles(CommonsFolder.ACTIVE.getFolder(), NameFolder.ACTIVE,
+                null);
+    }
+
+    /** TODO. */
+    public static void kafka() throws IOException {
+        AvroValidator.analiseFiles(CommonsFolder.KAFKA.getFolder(), NameFolder.KAFKA,
+                null);
+    }
+
+    /** TODO. */
+    public static void monitor() throws IOException {
+        AvroValidator.analiseFiles(CommonsFolder.MONITOR.getFolder(), NameFolder.MONITOR,
+                null);
+    }
+
+    /** TODO. */
+    public static void passive() throws IOException {
+        AvroValidator.analiseFiles(CommonsFolder.PASSIVE.getFolder(), NameFolder.PASSIVE,
+                null);
+    }
+}
---
+++ java-sdk/radar-schemas-validator/src/test/java/org/radarcns/validator/StructureValidator.java
@@ -0,0 +1,177 @@
+package org.radarcns.validator;
+
+/*
+ * Copyright 2017 King's College London and The Hyve
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.Test;
+
+/**
+ * Checks that the tree folder structure respects the following structure
+ * <ul>
+ *   <li>commons</li>
+ *    <ul>
+ *      <li>active</li>
+ *      <li>kafka</li>
+ *      <li>monitor</li>
+ *      <li>passive</li>
+ *    </ul>
+ *   <li>rest</li>
+ *   <li>specification</li>
+ *    <ul>
+ *      <li>active</li>
+ *      <li>monitor</li>
+ *      <li>passive</li>
+ *    </ul>
+ * </ul>
+ * At moment, the {@code restapi} does not have a well defined structure.
+ * TODO.
+ */
+public class StructureValidator {
+
+    /** Folder names. */
+    public enum NameFolder {
+        ACTIVE("active"),
+        COMMONS("commons"),
+        KAFKA("kafka"),
+        MONITOR("monitor"),
+        PASSIVE("passive"),
+        RESTAPI("restapi"),
+        SPECIFICATION("specification");
+
+        private final String name;
+
+        NameFolder(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    /** Root paths. */
+    public enum RootPath {
+        COMMONS(Paths.get(
+                  new File(".").toURI()).getParent().getParent().getParent().resolve(
+                      NameFolder.COMMONS.getName())),
+        REST_API(Paths.get(
+                  new File(".").toURI()).getParent().getParent().getParent().resolve(
+                      NameFolder.RESTAPI.getName())),
+        SPECIFICATION(Paths.get(
+                  new File(".").toURI()).getParent().getParent().getParent().resolve(
+                      NameFolder.SPECIFICATION.getName()));
+
+        private final Path path;
+
+        RootPath(Path path) {
+            this.path = path;
+        }
+
+        public Path getPath() {
+            return path;
+        }
+    }
+
+    /** Root folders. */
+    public enum RootFolder {
+        COMMONS(new File(RootPath.COMMONS.getPath().toUri())),
+        REST_API(new File(RootPath.REST_API.getPath().toUri())),
+        SPECIFICATION(new File(RootPath.SPECIFICATION.getPath().toUri()));
+
+        private final File folder;
+
+        RootFolder(File folder) {
+            this.folder = folder;
+        }
+
+        public File getFolder() {
+            return folder;
+        }
+    }
+
+    /** Commons folders. */
+    public enum CommonsFolder {
+        ACTIVE(new File(RootPath.COMMONS.getPath().resolve(NameFolder.ACTIVE.getName()).toUri())),
+        KAFKA(new File(RootPath.COMMONS.getPath().resolve(NameFolder.KAFKA.getName()).toUri())),
+        MONITOR(new File(RootPath.COMMONS.getPath().resolve(NameFolder.MONITOR.getName()).toUri())),
+        PASSIVE(new File(RootPath.COMMONS.getPath().resolve(NameFolder.PASSIVE.getName()).toUri()));
+
+        private final File folder;
+
+        CommonsFolder(File folder) {
+            this.folder = folder;
+        }
+
+        public File getFolder() {
+            return folder;
+        }
+    }
+
+    /** Commons folders. */
+    public enum SpecificationFolder {
+        ACTIVE(new File(RootPath.SPECIFICATION.getPath().resolve(
+              NameFolder.ACTIVE.getName()).toUri())),
+        MONITOR(new File(RootPath.SPECIFICATION.getPath().resolve(
+              NameFolder.MONITOR.getName()).toUri())),
+        PASSIVE(new File(RootPath.SPECIFICATION.getPath().resolve(
+              NameFolder.PASSIVE.getName()).toUri()));
+
+        private final File folder;
+
+        SpecificationFolder(File folder) {
+            this.folder = folder;
+        }
+
+        public File getFolder() {
+            return folder;
+        }
+    }
+
+    //private static final Logger LOGGER = LoggerFactory.getLogger(StructureValidator.class);
+
+    @Test
+    public void commons() throws IOException {
+        assertEquals(true, RootFolder.COMMONS.getFolder().isDirectory());
+
+        for (CommonsFolder folder : CommonsFolder.values()) {
+            assertEquals(true, folder.getFolder().isDirectory());
+        }
+
+        CommonsValidator.validateAll();
+    }
+
+    @Test
+    public void restapi() {
+        assertEquals(true, RootFolder.REST_API.getFolder().isDirectory());
+
+        //TODO check whether it is possible to define a structure
+    }
+
+    @Test
+    public void specification() {
+        assertEquals(true, RootFolder.SPECIFICATION.getFolder().isDirectory());
+
+        for (SpecificationFolder folder : SpecificationFolder.values()) {
+            assertEquals(true, folder.getFolder().isDirectory());
+        }
+    }
+}
