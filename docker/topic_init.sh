12d0451a64329a4e4e71ceeaeb1b35a6e7b2d349
Xi Bai <baixiac@gmail.com>
2021-07-22 16:56:20 +0100

    make retry on topic initialisation configurable

--- docker/topic_init.sh
+++ docker/topic_init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+MAX_RETRIES=5
+
 if [ -z NO_VALIDATE ]; then
   radar-schemas-tools validate merged
 fi
@@ -7,17 +9,21 @@ fi
 # Create topics
 echo "Creating RADAR-base topics..."
 
-if ! radar-schemas-tools create -c "${KAFKA_CONFIG_PATH}" -p $KAFKA_NUM_PARTITIONS -r $KAFKA_NUM_REPLICATION -b $KAFKA_NUM_BROKERS -s "${KAFKA_BOOTSTRAP_SERVERS}" merged; then
-  echo "FAILED TO CREATE TOPICS ... Retrying"
-  if ! radar-schemas-tools create -c "${KAFKA_CONFIG_PATH}" -p $KAFKA_NUM_PARTITIONS -r $KAFKA_NUM_REPLICATION -b $KAFKA_NUM_BROKERS -s "${KAFKA_BOOTSTRAP_SERVERS}" merged; then
-    echo "FAILED TO CREATE TOPICS"
-    exit 1
-  else
-    echo "Created topics at second attempt"
-  fi
-else
-  echo "Topics created."
-fi
+for ((i=1; i<=$MAX_RETRIES; i++)); do
+    if radar-schemas-tools create -c "${KAFKA_CONFIG_PATH}" -p $KAFKA_NUM_PARTITIONS -r $KAFKA_NUM_REPLICATION -b $KAFKA_NUM_BROKERS -s "${KAFKA_BOOTSTRAP_SERVERS}" merged; then
+        echo "Created topics at attempt $i"
+        break
+    else
+        if [ $i -eq $MAX_RETRIES ]; then
+            echo "FAILED TO CREATE TOPICS AFTER $i ATTEMPT(S)"
+            exit 1
+        else
+            echo "FAILED TO CREATE TOPICS ... Retrying"
+            sleep 10
+        fi
+    fi
+done
+echo "Topics created."
 
 echo "Registering RADAR-base schemas..."
 if ! radar-schemas-tools register --force -u "$SCHEMA_REGISTRY_API_KEY" -p "$SCHEMA_REGISTRY_API_SECRET" "${KAFKA_SCHEMA_REGISTRY}" merged; then
