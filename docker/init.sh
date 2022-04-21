a5733f81d6c730f1feaff04c6ff6465cdd3e7740
Joris Borgdorff <joris@thehyve.nl>
2022-04-21 16:35:48 +0200

    docker specifications exclude can be symlink

--- docker/init.sh
+++ docker/init.sh
@@ -6,7 +6,7 @@ rsync -a /schema/original/commons /schema/original/specifications /schema/merged
 rsync -a /schema/conf/ /schema/merged
 
 EXCLUDE_FILE=${EXCLUDE_FILE:-/etc/radar-schemas/specifications.exclude}
-if [ -f "$EXCLUDE_FILE" ]; then
+if [ -e "$EXCLUDE_FILE" ]; then
   while read -r exclude; do
     rm /schema/merged/specifications/$exclude
   done < "$EXCLUDE_FILE"
