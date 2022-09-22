59eb56139bd8c66d992978f86d74bcc2ba052c0d
Joris Borgdorff <joris@thehyve.nl>
2022-09-22 11:11:18 +0200

    Remove unused Gradle plugin references

--- java-sdk/settings.gradle.kts
+++ java-sdk/settings.gradle.kts
@@ -15,9 +15,6 @@ pluginManagement {
     plugins {
         kotlin("jvm") version kotlinVersion
         id("org.jetbrains.dokka") version dokkaVersion
-        id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
-        id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
-        id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
         id("io.github.gradle-nexus.publish-plugin") version nexusPluginVersion
         id("com.github.ben-manes.versions") version dependencyUpdateVersion
         id("com.github.davidmc24.gradle.plugin.avro-base") version avroGeneratorVersion
